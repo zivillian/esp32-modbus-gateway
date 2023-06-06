6e84510555cac939dc81db8e38386efb76f61680
Marco Garzola <zolamarco@gmail.com>
2023-06-06 17:06:31 +0200

    add wifi button reset

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include <ModbusClientRTU.h>
 #include "config.h"
 #include "pages.h"
+#include "Button2.h"
+
+
 
 AsyncWebServer webServer(80);
 Config config;
@@ -15,12 +18,44 @@ Preferences prefs;
 ModbusClientRTU *MBclient;
 ModbusBridgeWiFi MBbridge;
 WiFiManager wm;
+Button2 wifiButton;    
+
+// select which pin will trigger the configuration portal when set to LOW
+
+ const int timeout = 120; // seconds to run for
+#ifndef LONG_PRESS_TIME
+  #define LONG_PRESS_TIME  3000 // 4000 milliseconds
+#endif
+
+
+void onLongClick(Button2 &btn){
+   
+        // set configportal timeout
+        wm.setConfigPortalTimeout(timeout);
+
+        if (!wm.startConfigPortal()) {
+            dbgln("failed to connect and hit timeout");
+            delay(1000);
+            //reset and try again, or maybe put it to deep sleep
+            ESP.restart();
+        }
+ 
+        //if you get here you have connected to the WiFi
+        dbgln("connected to wifi...");
+}
+ 
+
 
 void setup() {
   debugSerial.begin(115200);
   dbgln();
   dbgln("[config] load")
   prefs.begin("modbusRtuGw");
+  #if defined(WIFI_RST_BUTTON_PIN)  
+    wifiButton.begin(WIFI_RST_BUTTON_PIN);
+    wifiButton.setLongClickDetectedHandler( &onLongClick);
+    wifiButton.setLongClickTime(LONG_PRESS_TIME);
+  #endif
   config.begin(&prefs);
   debugSerial.end();
   debugSerial.begin(config.getSerialBaudRate(), config.getSerialConfig());
@@ -55,6 +90,9 @@ void setup() {
   dbgln("[setup] finished");
 }
 
+
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  wifiButton.loop();
 }
